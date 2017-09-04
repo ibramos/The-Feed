330a06b3abb102b0cbda608228e8af156eaf776c
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-09-04 12:03:49 -0700

    added mongdoDB, authentication, and logout features

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
 
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+  cookieSession({
+    //cookie will last 30 days => (30days * 24hrs * 60mins * 60secs * 1000ms)
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //require returns a function which can be immediately invoked to take in the app argument
 require("./routes/oAuthRoutes")(app);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,13 +1,39 @@
 const passport = require("passport");
 const passportGoogleStrategy = require("passport-google-oauth20");
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+const User = mongoose.model("users");
+
+/**
+ * Generate identifying piece of information for users.
+ * @param {Object} user - Mongoose document obtained from database.
+ * @param {Function} done - Used to end function.
+ */
+passport.serializeUser((user, done) => {
+  /**
+   * Callback after every instance of passport usage.
+   * @param {Null} null
+   * @param {String} user.id - Generated based on MongoDB naming convention. Did not use googleID because oAuth is only used to create sign in. Internal identication will be used after oAuth confirmation.
+   */
+  done(null, user.id);
+});
+
+/**
+ * Transform identification to a Mongoose model.
+ * @param {String} id - Mongoose document id.
+ * @param {Function} done - Used to end function.
+ */
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 /**
  * Authenticate using Passport's Google Strategy.
- * @param {String} {clientID} - keys.googleClientID generated from Google+ API.
- * @param {String} {clientSecret} - keys.googleClientSecret generated from Google+ API.
+ * @param {String} keys.googleClientID - Generated from Google+ API.
+ * @param {String} keys.googleClientSecret - Generated from Google+ API.
  */
-
 passport.use(
   new passportGoogleStrategy(
     {
@@ -15,11 +41,23 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/oAuth/google/callback"
     },
+    /**
+     * Callback after Google oAuth confirmation.
+     * @param {String} accessToken - Google specific token after authentication.
+     * @param {String} refreshToken - Token to renew access.
+     * @param {Object} profile - Contains Google account information.
+     * @param {Function} done - Used to end function.
+     */
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken: ", accessToken);
-      console.log("refreshToken: ", refreshToken);
-      console.log("profile: ", profile);
-      console.log("done: ", done);
+      User.findOne({ googleID: profile.id }).then(userExists => {
+        if (userExists) {
+          done(null, userExists);
+        } else {
+          new User({ googleID: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
