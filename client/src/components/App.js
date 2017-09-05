98cb6cb469e6875479b0afdc196e9dd6d49d703d
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-09-05 14:39:24 -0700

    implement axios and redux thunk to create fetchUser action

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Header from './Header'
-import Dashboard from './Dashboard'
-import NewSurvey from './NewSurvey'
-import LandingPage from './LandingPage'
+import {connect} from "react-redux"
+import * as actions from '../actions/actions'
+import Header from "./Header";
+import Dashboard from "./Dashboard";
+import NewSurvey from "./NewSurvey";
+import LandingPage from "./LandingPage";
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={NewSurvey} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={LandingPage} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={NewSurvey} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
