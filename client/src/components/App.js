091fade89f0e298ce7d98b940f1c5abac41f21a9
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-09-05 11:32:34 -0700

    create react router architecture

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from './Header'
+import Dashboard from './Dashboard'
+import NewSurvey from './NewSurvey'
+import LandingPage from './LandingPage'
 
 const App = () => {
-  return <div> Hi </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={NewSurvey} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/reducers/combine.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import oAuthReducer from "./oAuthReducer";
+
+export default combineReducers({
+  oAuth: oAuthReducer
+});
