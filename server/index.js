a23c56b73bbf93654ee5711fef27e728eb5d3ffb
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-09-06 15:41:14 -0700

    path resolve fix in both static and sendfile

--- server/index.js
+++ server/index.js
@@ -28,11 +28,13 @@ require("./routes/oAuthRoutes")(app);
 require("./routes/paymentRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/../client/build"));
-
   const path = require("path");
+  app.use(express.static(path.resolve(__dirname, "..", "client", "build")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/../client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
   });
 }
 
