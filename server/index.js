6704781f8bc496da397bea1d6fc4a79a5a0dcdce
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-09-06 14:15:31 -0700

    fix pathing of express static

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ require("./routes/oAuthRoutes")(app);
 require("./routes/paymentRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("/../client/build"));
 
   const path = require("path");
   app.get("*", (req, res) => {
